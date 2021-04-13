ba687072cad1640a013029d8acb8e928034e17fd
Нефедов Никита <grand.marchello@gmail.com>
2021-04-13 15:37:58 +0400

    Не готовое задание 6(Через HashMap)

--- src/main/java/BookStore.java
+++ src/main/java/BookStore.java
@@ -1,66 +1,53 @@
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 public class BookStore {
-    MoneyCounter moneyCounter = new MoneyCounter();
-    Set<Integer> uniqKeys = new TreeSet<>();
-    List<Integer> notUniq = new ArrayList<>();
-    double count = 0; //Сумма денег
-    int counts = 0; //Количество уникальных книг
-    boolean first = true;  //Запуск только один раз
-    boolean minDiscount = false;
-
-    public double calculateBasketCost(List<Integer> books) {
-        if (first) {  //Так как List<Integer> books является не модифицируемым листом
-            first = false; //то забиваем его в наш лист.
-            notUniq.addAll(books);
-        }
-        uniqKeys.addAll(notUniq); //Переносим все значения в TreeSet, благодаря чему получаем только уникальные значения, без повторений
-        for (int i = 0; i < uniqKeys.toArray().length; i++) {
-            if (i == 4 && books.size() % 8 == 0) { //Если размер корзины делится с остатком на 8, с большой вероятностью
-                this.count += moneyCounter.counter(counts); //она выгоднее по 4 штуки в связке
-                minDiscount = true;
-                counts = 0;
-                break;
-            }
-            counts++; //Прибавляем книгу в "связку" уникальных книг
-            notUniq.remove(uniqKeys.toArray()[i]); //Удаляем уникальное число из корзины
-        }
-        if (minDiscount) {
-            minDiscount = false;
-        } else {
-            this.count += moneyCounter.counter(counts);
-            counts = 0;
-        }
-        uniqKeys.clear();
-        if (notUniq.size() > 0) {
-            calculateBasketCost(notUniq); //Если массив еще полностью не очищен, запускаем заново.
-        }
-        return this.count; //Возвращаем сумму
-    }
-
-}
-/* Попытка сделать через HashMap
+    MoneyCounter check = new MoneyCounter();
     int count = 0;
     int finalPrice = 0;
-    MoneyCounter check = new MoneyCounter();
-    List<Integer> books = Arrays.asList(1, 1, 2, 3, 3, 3, 5, 4, 4, 5, 1);
+    int y = 0;
+    int size = 0;
+    boolean first = true;
     HashMap<Integer, Integer> copyingOfBooks = new HashMap();
-        for (Integer b : books) {
-                int newValue = copyingOfBooks.getOrDefault(b, 0) + 1;
-                copyingOfBooks.put(b, newValue);
-                }
-public void doIt(HashMap<Integer, Integer> copyingOfBooks){
-        for (int i = 1; i < copyingOfBooks.size() + 1; i++) {
-        System.out.println(copyingOfBooks.values());
-        while (copyingOfBooks.containsKey(i) && copyingOfBooks.get(i) > 1) {
-        count++;
-        copyingOfBooks.replace(i, copyingOfBooks.get(i), copyingOfBooks.get(i) - 1);
-        continue;
-        }
-        if(!copyingOfBooks.containsValue(2)){
-        count++;
-        copyingOfBooks.replace(i, copyingOfBooks.get(i), copyingOfBooks.get(i) - 1);}
 
-        //else{copyingOfBooks.replace(i, copyingOfBooks.get(i), copyingOfBooks.get(i) - 1);count++;}
+    public int calculateBasketCost(List<Integer> books) {
+        if(books.isEmpty())
+            return 0;
+        if(first){
+        for (Integer b : books) {
+            size++;
+            int newValue = copyingOfBooks.getOrDefault(b, 0) + 1;
+            copyingOfBooks.put(b, newValue);
         }
-        System.out.println(copyingOfBooks);} */
+        first = false;
+        }                        //здесь
+        while(count < Collections.max(copyingOfBooks.values())){
+            for (int i = 1; i < copyingOfBooks.size()+1; i++) {
+                if(size % 8 == 0 && count == 4){
+                    finalPrice += check.counter(count);
+                    count = 0;
+                    break;}
+                while (copyingOfBooks.containsKey(i) && copyingOfBooks.get(i) > 1) {
+                    y = i;
+                    count++;
+                    copyingOfBooks.replace(i, copyingOfBooks.get(i), copyingOfBooks.get(i) - 1);
+                    break;
+                }
+                if(count==5)
+                    break;
+                if (!copyingOfBooks.containsValue(2) && !copyingOfBooks.containsValue(3) && copyingOfBooks.get(i) != 0 && y != i) {
+                    copyingOfBooks.replace(i, copyingOfBooks.get(i), copyingOfBooks.get(i) - 1);
+                    count++;
+                }
+                y=0;
+
+
+            }}
+        finalPrice += check.counter(count);
+        count = 0;
+        if (Collections.max(copyingOfBooks.values()) > 0)
+            calculateBasketCost(books);
+        return finalPrice;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<Integer> books = Arrays.asList(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1);
         BookStore bookStore = new BookStore();
-        List<Integer> books = Arrays.asList(1, 1, 2, 3, 4, 4, 5, 5);
+        //HashMap<Integer, Integer> copyingOfBooks = new HashMap();
+            //for (Integer b : books) {
+               // int newValue = copyingOfBooks.getOrDefault(b, 0) + 1;
+                //copyingOfBooks.put(b, newValue);}
         System.out.println(bookStore.calculateBasketCost(books));
+
     }
 }
